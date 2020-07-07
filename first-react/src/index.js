ab6bd62380fd85cacd84e39c1c8cd2b8d5a5864a
kimth007kim <kimth007kim@naver.com>
2020-07-07 17:39:31 +0900

    2강 (실습) JSX 코드 작성해보기 완료

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
+const formatName = function (me) {
+  return me.first + " " + me.last;
+};
+
+const me = {
+  first: "kyung dong",
+  last: "kim",
+};
+const element = <h1>Hello, {formatName(me)}!</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
