6c93bf3bb8fc108823a53d79fb363bb425cee76e
kimth007kim <kimth007kim@naver.com>
2020-07-08 15:40:30 +0900

    3강 (실습) 시계 만들기 완료

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -4,24 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
 
-const formatName = function (me) {
-  return me.first + " " + me.last;
-};
+  ReactDOM.render(element, document.getElementById("root"));
+}
+setInterval(tick, 1000);
 
-const me = {
-  first: "kyung dong",
-  last: "kim",
-};
-const element = <h1>Hello, {formatName(me)}!</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
