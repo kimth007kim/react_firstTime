80a383f9759b149a28a9253b28496f406d8779b1
kimth007kim <kimth007kim@naver.com>
2020-07-10 16:17:19 +0900

    4강 (실습) 댓글 컴포넌트 만들기 완료

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -3,9 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Comment from "./Comment";
 
+const comments = [
+  { name: "Tony Start", content: `"I am IronMan"` },
+  { name: "Peter Parker", content: `"A neighborhood with a kindness"` },
+];
+
 function App() {
   return (
-    <div className="App" style={{ padding: 16, backgroundColor: "000000" }}>
+    <div className="App" style={{ padding: 16, backgroundColor: "#282c34" }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,7 +26,9 @@ function App() {
         </a>
       </header>
       <div>
-        <Comment />
+        {comments.map((comment, index) => {
+          return <Comment name={comment.name} content={comment.content} />;
+        })}
       </div>
     </div>
   );
--- first-react/src/Comment.js
+++ first-react/src/Comment.js
@@ -1,14 +1,55 @@
 import React from "react";
 
 const styles = {
-  root: { color: "red" },
+  root: {
+    width: "20%",
+    margin: "auto",
+    marginTop: 16,
+    padding: 16,
+    textAlign: "left",
+    backgroundColor: "white",
+    borderRadius: 16,
+  },
+  imageContainer: {
+    display: "inline-block",
+    width: "50",
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  commentContainer: {
+    display: "inline-block",
+    marginLeft: "16",
+    textAlign: "left",
+    verticalAlign: "top",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 20,
+    fontweight: "700",
+  },
+  contentText: {
+    color: "black",
+    fontSize: 16,
+  },
 };
 
 class Comment extends React.Component {
   render() {
     return (
       <div style={styles.root}>
-        <h1>{"My first component!"}</h1>
+        <div style={styles.imageContainer}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+            style={styles.image}
+          />
+        </div>
+        <div style={styles.commentContainer}>
+          <div style={styles.nameText}>{this.props.name}</div>
+          <span style={styles.contentText}>{this.props.content}</span>
+        </div>
       </div>
     );
   }
